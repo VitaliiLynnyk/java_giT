4d418e3605b319c61d2781655a60f2e26e304780
VitaliiLynnyk <linnik2101@gmail.com>
2016-09-23 12:46:24 +0300

    lab1_result

--- 1.java
+++ 1.java
@@ -1,21 +1,22 @@
+package com.company;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
- class main {
+class Main {
 
     public static void main(String[] args) {
         JFrame window = new JFrame("first laba");
-        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        window.setSize(400,400);
+		 window.setSize(400,400);
+        window.setDefaultCloseOperation(window.EXIT_ON_CLOSE);
         window.setVisible(true);
         JButton btn = new JButton("start");
         JTextField str1 = new JTextField();
         JTextField res = new JTextField();
         //створення компонентів
-        window.getContentPane().add(str1);
-        window.getContentPane().add(btn);
-        window.getContentPane().add(res);
+        window.getContentPane().add(str1,BorderLayout.CENTER);
+        window.getContentPane().add(btn,BorderLayout.EAST);
+        window.getContentPane().add(res,BorderLayout.SOUTH);
 
         btn.addActionListener(new ActionListener() {
             @Override
