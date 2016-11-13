3ed3368b34897c4a4ffd29d5e07656647ffb7972
VitaliiLynnyk <linnik2101@gmail.com>
2016-11-13 21:54:11 +0200

    lab5/start

---
+++ laba5/src/com/company/Main.java
@@ -0,0 +1,36 @@
+package com.company;
+
+public class Main {
+
+
+   public class Pens
+    {
+        public  String Color ;
+        public  String Brend;
+        public  String TypeOfPen;
+
+        Pens()
+        {
+            Color = "Blue";
+            Brend = "Lecce_Pen";
+            TypeOfPen = "feather";
+        }
+    }
+
+
+
+   public class DeviceOfWriting extends  Pens
+    {
+        public  String 
+    }
+
+
+
+
+
+
+    public static void main(String[] args)
+    {
+
+    }
+}
